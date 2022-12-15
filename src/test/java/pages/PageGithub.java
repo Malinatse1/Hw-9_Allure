d8c4c8dfb11331d5a43a5e89c952a0c2a295148e
Natalya Bochkova <bochkovanatalya7@gmail.com>
2022-12-15 19:50:00 +0300

    Add tests

---
+++ src/test/java/pages/PageGithub.java
@@ -0,0 +1,10 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class PageGithub {
+
+    public SelenideElement searchInput =  $(".header-search-input");
+}
---
+++ src/test/java/tests/SelenideSimpleTest.java
@@ -0,0 +1,36 @@
+        package tests;
+
+
+        import com.codeborne.selenide.Condition;
+        import com.codeborne.selenide.SelenideElement;
+        import com.codeborne.selenide.logevents.SelenideLogger;
+        import io.qameta.allure.selenide.AllureSelenide;
+        import org.junit.jupiter.api.Test;
+        import pages.PageGithub;
+
+
+        import static com.codeborne.selenide.Selectors.withText;
+        import static com.codeborne.selenide.Selenide.$;
+        import static com.codeborne.selenide.Selenide.open;
+        import static org.openqa.selenium.By.linkText;
+
+
+
+        public class SelenideSimpleTest extends PageGithub {
+        @Test
+        public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com/");
+
+        searchInput.click();
+        searchInput.sendKeys("Malinatse1/Hw_9_Allure");
+        searchInput.submit();
+
+        $(linkText("Malinatse1/Hw_9_Allure")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
+
+
+
+        }
+        }
